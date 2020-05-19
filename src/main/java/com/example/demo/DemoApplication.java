922b56686bcdf78a7e13dd993168d7b11c21eac4
jmascuf <jacobo.m.f@outlook.com>
2020-05-19 18:04:40 +0200

    social login

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,27 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends WebSecurityConfigurerAdapter{
+	 @Override
+	    protected void configure(HttpSecurity http) throws Exception {
+	    	// @formatter:off
+	        http
+	            .authorizeRequests(a -> a
+	                .antMatchers("/", "/backend/users", "/error", "/webjars/**").permitAll()
+	                .anyRequest().authenticated()
+	            )
+	            .exceptionHandling(e -> e
+	                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+	            )
+	            .oauth2Login();
+	        // @formatter:on
+	    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
