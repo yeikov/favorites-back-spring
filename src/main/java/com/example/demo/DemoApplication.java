de7bd2e4bae723d20c619814d6e614b7fd8b1649
jmascuf <jacobo.m.f@outlook.com>
2020-05-20 18:28:49 +0200

    social login

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,29 +6,48 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import io.swagger.models.HttpMethod;
 
 @SpringBootApplication
-public class DemoApplication extends WebSecurityConfigurerAdapter{
-	 @Override
-	    protected void configure(HttpSecurity http) throws Exception {
-	    	// @formatter:off
-	        http
-	            .authorizeRequests(a -> a
-	                .antMatchers("/", "/backend/users", "/error", "/webjars/**").permitAll()
-	                .anyRequest().authenticated()
-	            )
-	            .exceptionHandling(e -> e
-	                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-	            )
-	            .oauth2Login();
-	        // @formatter:on
-	    }
+public class DemoApplication extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http.authorizeRequests(a -> a.antMatchers("/error", "/webjars/**"
+
+		).permitAll()
+				
+				.mvcMatchers( 
+						backEndUrl,
+						backEndUrl + "/users/{id}", 
+						backEndUrl + "/users",
+						backEndUrl + "/registries/{id}", 
+						backEndUrl + "/registries", 
+						backEndUrl + "/assessments/{id}",
+						backEndUrl + "/assessments"
+
+				).permitAll()
+				.anyRequest().authenticated()
+
+		).exceptionHandling(e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+				.oauth2Login();
+		// @formatter:on
+	}
+	
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+          .addResourceHandler("/resources/**")
+          .addResourceLocations("/resources/"); 
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
 	public static final String backEndUrl = "/backend";
-	
-	
-	
+
 }
