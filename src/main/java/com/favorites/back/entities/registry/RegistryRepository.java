17c6c02e7ec0fa849a1a2d87c748259a041bacad
yeikov <jacobo.m.f@gmail.com>
2024-10-13 11:35:40 +0200

    query fixes

--- src/main/java/com/favorites/back/entities/registry/RegistryRepository.java
+++ src/main/java/com/favorites/back/entities/registry/RegistryRepository.java
@@ -9,11 +9,11 @@ import org.springframework.data.jpa.repository.Query;
 public interface RegistryRepository extends JpaRepository<Registry, Long>{
 	public List <Registry> findAllByTitleAndMediaAndAuthorAndProductionDate(String title, String media,String author, LocalDate year);
 
-	@Query(value = "SELECT DISTINCT id, title, author, media, production_date FROM (SELECT r.id, r.title, r.author, r.media, r.production_date, a.registry_id, a.favorite FROM registry AS r INNER JOIN assessment AS a ON r.id = a.registry_id AND r.media = :media GROUP BY r.title, a.id ORDER BY a.favorite DESC) LIMIT 8;",
+ 	@Query(value = "SELECT DISTINCT id, title, author, media, production_date FROM (SELECT r.id, r.title, r.author, r.media, r.production_date, a.registry_id, a.favorite FROM registry AS r INNER JOIN assessment AS a ON r.id = a.registry_id AND r.media = :media GROUP BY r.title, a.id ORDER BY a.favorite DESC) AS dt LIMIT 8;",
 			nativeQuery = true)
 	public List <Registry> findTopFavoriteByMedia(String media);
 	
-	@Query(value = "SELECT DISTINCT id, title, author, media, production_date FROM (SELECT r.id, r.title, r.author, r.media, r.production_date, a.registry_id, a.recommend FROM registry AS r INNER JOIN assessment AS a ON r.id = a.registry_id AND r.media = :media GROUP BY r.title, a.id ORDER BY a.recommend DESC) LIMIT 8;",
+	@Query(value = "SELECT DISTINCT id, title, author, media, production_date FROM (SELECT r.id, r.title, r.author, r.media, r.production_date, a.registry_id, a.recommend FROM registry AS r INNER JOIN assessment AS a ON r.id = a.registry_id AND r.media = :media GROUP BY r.title, a.id ORDER BY a.recommend DESC) AS dt LIMIT 8;",
 			nativeQuery = true)
 	public List <Registry> findTopRecommendByMedia(String media);
 	
