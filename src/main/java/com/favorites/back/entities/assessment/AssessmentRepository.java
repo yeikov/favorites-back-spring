713bb1f073e8da6fc42ce1ede66e1732ff4cb6ca
yeikov <jacobo.m.f@gmail.com>
2024-12-11 18:20:26 +0100

    assessments by registry - pagination

--- src/main/java/com/favorites/back/entities/assessment/AssessmentRepository.java
+++ src/main/java/com/favorites/back/entities/assessment/AssessmentRepository.java
@@ -3,35 +3,33 @@ package com.favorites.back.entities.assessment;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.favorites.back.entities.registry.Registry;
 import com.favorites.back.entities.viewer.Viewer;
 
-public interface AssessmentRepository extends JpaRepository <Assessment, Long>{
-	
-	public List <Assessment> findAllByViewer(Viewer viewer);
-	
-	public List <Assessment> findAllByRegistry(Registry registry);
-	
-	public Iterable <Assessment> findAllByViewerAndRegistry(Viewer viewer, Registry registry);
-	
+public interface AssessmentRepository extends JpaRepository<Assessment, Long> {
+
+	public List<Assessment> findAllByViewer(Viewer viewer);
+
+	public Page<Assessment> findAllByRegistry(Registry registry, Pageable pageable);
+
+	public Iterable<Assessment> findAllByViewerAndRegistry(Viewer viewer, Registry registry);
+
 	@SuppressWarnings("null")
-	public Optional <Assessment> findById(Long id);
-	
-	public List <Assessment> findAllByRegistryMedia(String media);
-	
-	@Query(
-			value = "SELECT a.* FROM assessment AS a INNER JOIN registry AS r ON a.registry_id=r.id AND r.media=:media ORDER BY a.favorite DESC LIMIT 8;",
-			nativeQuery = true
-			)
-	public List <Assessment> findM(String media);
-	
-	
-	@Query(
-			value = "SELECT a.* FROM assessment AS a INNER JOIN registry AS r ON a.registry_id=r.id AND a.viewer_id=:viewerId AND r.media=:media ORDER BY a.favorite DESC;", 
-			nativeQuery = true)
-	public List <Assessment> findUM(Long viewerId, String media);
+	public Optional<Assessment> findById(Long id);
+
+	public List<Assessment> findAllByRegistryMedia(String media);
+
+	@Query(value = "SELECT a.* FROM assessment AS a INNER JOIN registry AS r ON a.registry_id=r.id AND r.media=:media ORDER BY a.favorite DESC LIMIT 8;", nativeQuery = true)
+	public List<Assessment> findM(String media);
+
+	@Query(value = "SELECT a.* FROM assessment AS a INNER JOIN registry AS r ON a.registry_id=r.id AND a.viewer_id=:viewerId AND r.media=:media ORDER BY a.favorite DESC;", nativeQuery = true)
+	public List<Assessment> findUM(Long viewerId, String media);
 }
-//"SELECT a.*, r.media, r.author, r.title, r.id as od FROM assessment AS a INNER JOIN registry AS r ON a.registry_id=r.id AND a.viewer_id=:viewerId AND r.media=':media' ORDER BY a.favorite DESC;", 
+// "SELECT a.*, r.media, r.author, r.title, r.id as od FROM assessment AS a
+// INNER JOIN registry AS r ON a.registry_id=r.id AND a.viewer_id=:viewerId AND
+// r.media=':media' ORDER BY a.favorite DESC;",
